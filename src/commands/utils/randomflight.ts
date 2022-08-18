c89438ec465e485f4d3b0849d8f8ecd0a2fa0171
Imenes <kaporr@gmail.com>
2022-08-19 01:41:41 +0200

    Update randomflight.ts

--- src/commands/utils/randomflight.ts
+++ src/commands/utils/randomflight.ts
@@ -12,57 +12,80 @@ export const randomFlight: CommandDefinition = {
         const query = msg.content.replace(/\.(destinations|dest|random|randomflight)/, ' ').toLowerCase().split(' ').join('');
 
         const europe = [
-            'LJLJ - LYTV',
-            'LJLJ - LDDU',
-            'LJLJ - LSZH',
-            'LJLJ - LFKJ',
-            'LJLJ - LIRF',
-            'LJLJ - EDDF',
-            'LJLJ - LYPG',
-
-            'LYTV - LJLJ',
-            'LDDU - LJLJ',
-            'LSZH - LJLJ',
-            'LFKJ - LJLJ',
-            'LIRF - LJLJ',
-            'EDDF - LJLJ',
+            'LJLJ - LYTV', 'LYTV - LJLJ', 'LDZA - LDDU', 'LDDU - LDZA',
+            'LJLJ - LDDU', 'LDDU - LJLJ', 'LDZA - EDDF', 'EDDF - LDZA',
+            'LJLJ - LSZH', 'LSZH - LJLJ', 'LDZA - LIRF', 'LIRF - LDZA',
+            'LJLJ - LFKJ', 'LFKJ - LJLJ', 'LDZA - LYTV', 'LYTV - LDZA',
+            'LJLJ - LIRF', 'LIRF - LJLJ', 'LDZA - LSZH', 'LSZH - LDZA',
+            'LJLJ - EDDF', 'EDDF - LJLJ', 'LDZA - LFKJ', 'LFKJ - LDZA',
+            'LJLJ - LYPG', 'LYPG - LJLJ', 'LDZA - LOWW', 'LOWW - LDZA',
+            'LJLJ - LYBE', 'LYBE - LJLJ', 'LDZA - LYBE', 'LYBE - LDZA',
+            'LJLJ - LOWW', 'LOWW - LJLJ', 'LDZA - LYPG', 'LYPG - LDZA',
         ];
         const northAmerica = [
-            'KLAS - KLAX',
-            'KLAS - KSJC',
-            'KLAS - KPDX',
-            'KLAS - KBOI',
-            'KLAS - KMIA',
-            'KLAS - KATL',
-
-            'KLAX - KLAS',
-            'KSJC - KLAS',
-            'KPDX - KLAS',
-            'KBOI - KLAS',
-            'KMIA - KLAS',
-            'KATL - KLAS',
+            'KLAS - KLAX', 'KLAX - KLAS', 'KLAX - KSEA', 'KSEA - KLAX',
+            'KLAS - KSJC', 'KSJC - KLAS', 'KLAX - KPDX', 'KPDX - KLAX',
+            'KLAS - KPDX', 'KPDX - KLAS', 'KLAX - MMLT', 'MMLT - KLAX',
+            'KLAS - KBOI', 'KBOI - KLAS', 'KLAX - KABQ', 'KABQ - KLAX',
+            'KLAS - KMIA', 'KMIA - KLAS', 'KLAX - KDEN', 'KDEN - KLAX',
+            'KLAS - KATL', 'KATL - KLAS', 'KLAX - KGEG', 'KGEG - KLAX',
+            'KLAS - KSAN', 'KSAN - KLAS', 'KLAX - KSMF', 'KSMF - KLAX',
+            'KLAS - CYYC', 'CYYC - KLAS', 'KLAX - KELP', 'KELP - KLAX',
+            'KLAS - KSEA', 'KSEA - KLAS', 'KLAX - KSLC', 'KSLC - KLAX',
         ];
         const africa = [
-            'HECA - OEJN',
-            'HECA - OMDB',
-            'HECA - DTTA',
-
-            'OEJN - HECA',
-            'OMDB - HECA',
-            'DTTA - HECA',
-
+            'HECA - OEJN', 'OEJN - HECA', 'HEGN - OEJN', 'OEJN - HEGN',
+            'HECA - OMDB', 'OMDB - HECA', 'HEGN - OERK', 'OERK - HEGN',
+            'HECA - DTTA', 'DTTA - HECA', 'HEGN - HEBA', 'HEBA - HEGN',
+            'HECA - OERK', 'OERK - HECA', 'HEGN - UDYZ', 'UDYZ - HEGN',
+            'HECA - HESH', 'HESH - HECA', 'HEGN - LTFJ', 'LTFJ - HEGN',
+            'HECA - HEGN', 'HEGN - HECA', 'HEGN - LTFM', 'LTFM - HEGN',
+            'HECA - HAAB', 'HAAB - HECA', 'HEGN - URMM', 'URMM - HEGN',
+            'HECA - FTTJ', 'FTTJ - HECA', 'HEGN - LWSK', 'LWSK - HEGN',
         ];
-        const random = europe.concat(northAmerica, africa);
+        const asia = [
+            'RJTT - RJCC', 'RJCC - RJTT', 'RJOO - RJSS', 'RJSS - RJOO',
+            'RJTT - RJFF', 'RJFF - RJTT', 'RJOO - RJFK', 'RJFK - RJOO',
+            'RJTT - RJFK', 'RJFK - RJTT', 'RJOO - RJFM', 'RJFM - RJOO',
+            'RJTT - RJFM', 'RJFM - RJTT', 'RJOO - RJCC', 'RJCC - RJOO',
+            'RJTT - RJOA', 'RJOA - RJTT', 'RJOO - RJFT', 'RJFT - RJOO',
+            'RJTT - ROAH', 'ROAH - RJTT', 'RJOO - RJSN', 'RJSN - RJOO',
+            'RJTT - RKSS', 'RKSS - RJTT', 'RJOO - RJOM', 'RJOM - RJOO',
+            'RJTT - RJOO', 'RJOO - RJTT', 'RJOO - RJFF', 'RJFF - RJOO',
+        ];
+        const australia = [
+            'YSSY - YBNA', 'YBNA - YSSY', 'YMML - YCFS', 'YCFS - YMML',
+            'YSSY - YMML', 'YMML - YSSY', 'YMML - YBCG', 'YBCG - YMML',
+            'YSSY - YBBN', 'YBBN - YSSY', 'YMML - YPAD', 'YPAD - YMML',
+            'YSSY - YSCB', 'YSCB - YSSY', 'YMML - YMHB', 'YMHB - YMML',
+            'YSSY - YPAD', 'YPAD - YSSY', 'YMML - YSCB', 'YSCB - YMML',
+            'YSSY - YPPH', 'YPPH - YSSY', 'YMML - NZAA', 'NZAA - YMML',
+            'YSSY - YCFS', 'YCFS - YSSY', 'YMML - YBBN', 'YBBN - YMML',
+            'YSSY - YBSU', 'YBSU - YSSY', 'YMML - YPPH', 'YPPH - YMML',
+            'YSSY - YBCG', 'YBCG - YSSY', 'YMML - YBCS', 'YBCS - YMML',
+            'YSSY - YMHB', 'YMHB - YSSY', 'YMML - YBAS', 'YBAS - YMML',
+            'YSSY - NZAA', 'NZAA - YSSY', 'YMML - YAYE', 'YAYE - YMML',
+            'YSSY - YBCS', 'YBCS - YSSY', 'YMML - YBRM', 'YBRM - YMML',
+            'YSSY - YBAS', 'YBAS - YSSY', 'YMML - YPDN', 'YPDN - YMML',
+            'YSSY - YAYE', 'YAYE - YSSY', 'YMML - YBTL', 'YBTL - YMML',
+            'YSSY - YBTL', 'YBTL - YSSY', 'YMML - YBPN', 'YBPN - YMML',
+            'YSSY - YBPN', 'YBPN - YSSY', 'YMML - YBHM', 'YBHM - YMML',
+        ];
+        const random = europe.concat(northAmerica, africa, asia, australia);
 
         const randomFlight = (random[Math.floor(Math.random() * random.length)]);
         const europanFlight = (europe[Math.floor(Math.random() * europe.length)]);
         const naFlight = (northAmerica[Math.floor(Math.random() * northAmerica.length)]);
         const africanFlight = (africa[Math.floor(Math.random() * africa.length)]);
+        const asianFlight = (asia[Math.floor(Math.random() * asia.length)]);
+        const australianFlight = (australia[Math.floor(Math.random() * australia.length)]);
 
         const randomFlightSelected = randomFlight.split(' ');
         const europanFlightSelected = europanFlight.split(' ');
         const naFlightSelected = naFlight.split(' ');
-        const africanSelected = africanFlight.split(' ');
+        const africanFlightSelected = africanFlight.split(' ');
+        const asianFlightSelected = asianFlight.split(' ');
+        const australianFlightSelected = australianFlight.split(' ');
 
         const randomFlightEmbed = makeEmbed({
             title: 'Random Flight',
@@ -93,13 +116,25 @@ export const randomFlight: CommandDefinition = {
             description: makeLines([
                 (africanFlight),
                 ' ',
-                `Dispatch via [SimBrief](${SIMBRIEF_BASE_URL}${africanSelected[0]}&dest=${africanSelected[2]})`,
+                `Dispatch via [SimBrief](${SIMBRIEF_BASE_URL}${africanFlightSelected[0]}&dest=${africanFlightSelected[2]})`,
             ]),
         });
-
-        //query.shift();
-        //const continent = query.join('');
-        //const continent = query[1] + query[2];
+        const asiaFlightEmbed = makeEmbed({
+            title: 'Random Asian Flight',
+            description: makeLines([
+                (asianFlight),
+                ' ',
+                `Dispatch via [SimBrief](${SIMBRIEF_BASE_URL}${asianFlightSelected[0]}&dest=${asianFlightSelected[2]})`,
+            ]),
+        });
+        const australianFlightEmbed = makeEmbed({
+            title: 'Random Australian Flight',
+            description: makeLines([
+                (australianFlight),
+                ' ',
+                `Dispatch via [SimBrief](${SIMBRIEF_BASE_URL}${australianFlightSelected[0]}&dest=${australianFlightSelected[2]})`,
+            ]),
+        });
 
         if (query.length === 0) {
             await msg.channel.send({ embeds: [randomFlightEmbed] });
@@ -109,6 +144,10 @@ export const randomFlight: CommandDefinition = {
             await msg.channel.send({ embeds: [northAmericaFlightEmbed] });
         } else if (query === 'africa' || query === 'af') {
             await msg.channel.send({ embeds: [africaFlightEmbed] });
+        } else if (query === 'asia' || query === 'as') {
+            await msg.channel.send({ embeds: [asiaFlightEmbed] });
+        } else if (query === 'australia' || query === 'au') {
+            await msg.channel.send({ embeds: [australianFlightEmbed] });
         } else {
             return msg.reply(`"${randomFlightSelected[0]}" is not a valid continent!`);
         }
